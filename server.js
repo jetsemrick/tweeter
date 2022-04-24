28310edbe0d1d6acf49e5258b6068687f2a02fbb
Nathan Dodson <nathanrdodson@gmail.com>
2022-04-23 23:20:27 -0500

    Begin edit profile implemenation

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var mysql = require('mysql');
 const bcrypt = require("bcrypt");
 
 const HTTP_PORT = 3000
+const SALT_ROUND = 12;
 
 var session;
 
@@ -91,8 +92,7 @@ app.post('/register', async (req, res) => {
       res.send("Username exceeds maximum length allowed. Please <a href='/register'>go back</a> and try something different.");
     } else {
       // otherwise, go ahead and make the insert, redirect the user to the home page to try and log in.
-	  const saltRound = 12;
-	  bcrypt.hash(req.body.password, saltRound).then((hash) => {
+	  bcrypt.hash(req.body.password, SALT_ROUND).then((hash) => {
       connection.query(`
       INSERT INTO Users (username, email, password, created_on) 
       VALUES ('${req.body.username}', '${req.body.email}', '${hash}', NOW())`, function(err, result, fields) {
@@ -112,6 +112,141 @@ app.get('/profile', (req, res) => {
   } else {
     res.sendFile('/www/profile.html', { root :__dirname });
   }
+});
+
+app.get('/profile/edit', (req, res) => {
+  session = req.session;
+  if(!session.uid) {
+    res.sendFile('/www/login.html', { root :__dirname });
+  } else {
+    res.sendFile('/www/edit_profile.html', { root :__dirname });
+  }
+});
+
+app.get('/editProfile', (req, res) => {
+  // pull tweets: SELECT post_id,post_content,post_likes,username FROM Posts INNER JOIN Users ON Posts.uid=Users.uid AND Users.uid=3
+  // pull user details for card
+  connection.query(`
+  SELECT * FROM Users 
+  WHERE username='${req.session.uid}'`, function(err, profile, fields) {
+    if(err) throw err;
+
+    res.send({
+      profile: profile
+    });
+
+  });
+});
+
+app.post('/editProfile', (req, res) => {
+  let pass, username, fullname, email, bio;
+
+  console.log("Starting initial user query");
+
+  connection.query(`
+  SELECT * FROM Users 
+  WHERE username='${req.session.uid}'`, function(err, result, fields) {
+    if(err) throw err;
+
+    if(!bcrypt.compareSync(req.body.password, result[0].password))
+      res.send("Incorrect password. Please <a href='/register'>go back</a> and try again.");
+
+    pass = result[0].password;
+    username = result[0].username;
+    fullname = result[0].pref_name;
+    email = result[0].email;
+    bio = result[0].bio;
+
+    console.log("Starting edit checks...");
+    return;
+  });
+
+    console.log(req.body);
+
+    if(req.body.username != "") {
+
+      console.log("Attempting to query username for check"); // LOGGING
+      connection.query(`SELECT username FROM Users WHERE username='${req.body.username}'`, function(err, result, fields) {
+        if(err) throw err;
+
+        if(result.length > 0) {
+          console.log("Username already in use"); // LOGGING
+          res.send("Username already in use. Please <a href='/edit/profile'>go back</a> and try something different.");
+        } else if(String(req.body.username).length > 12) {
+          console.log("Username too long"); // LOGGING
+          res.send("Username exceeds maximum length allowed. Please <a href='/register'>go back</a> and try something different.");
+        }
+
+        username = req.body.username;
+        return;
+      });
+    } 
+    
+    if(req.body.pref_name != "") {
+      if(String(req.body.pref_name).length > 20) {
+        console.log("Full Name too long"); // LOGGING
+        res.send("Full name exceeds maximum allowed length of 20 characters. Please <a href='/register'>go back</a> and try something different.");
+      }
+
+      fullname = req.body.pref_name;
+    } 
+    
+    if(req.body.email != "") {
+      console.log("Attempting to query email for check"); // LOGGING
+      connection.query(`SELECT email FROM Users WHERE email='${req.body.email}'`, function(err, result, fields) {
+        if(err) throw err;
+
+        if(result.length > 0) {
+          console.log("Email already in use"); // LOGGING
+          res.send("Email already in use. Please <a href='/edit/profile'>go back</a> and try something different.");
+        } else if(String(req.body.email).length > 100) {
+          console.log("Email too long"); // LOGGING
+          res.send("Email exceeds maximum allowed length of 100 characters. Please <a href='/register'>go back</a> and try something different.");
+        }
+        return
+      });
+
+      email = req.body.email;
+    } 
+    
+    if(req.body.bio != "")
+    {
+      if(String(req.body.bio).length > 160) {
+          res.send("Bio exceeds maximum allowed length of 160 characters. Please <a href='/register'>go back</a> and try something different.");
+      }
+
+      bio = req.body.bio;
+    } 
+    
+    if(req.body.newPassword != "")
+    {
+      console.log("Starting password check") // LOGGING
+      if(String(req.body.newPassword).length > 100) {
+          console.log("New password too long"); // LOGGING
+          res.send("Password exceeds maximum allowed length of 160 characters. Please <a href='/register'>go back</a> and try something different.");
+      } else {
+        console.log("Attempting to create new password"); // LOGGING
+        bcrypt.hash(req.body.newPassword, SALT_ROUND).then(function(hash) {
+          pass = hash;
+          console.log("Created new password"); // LOGGING
+          return;
+        });
+        console.log("Got here"); // LOGGING
+      }
+    }
+
+    console.log("Attempting query");
+    connection.query(`
+    UPDATE Users
+    SET username = '${username}', password = '${pass}', email = '${email}', pref_name = '${fullname}', bio = '${bio}'
+    WHERE username = '${req.session.uid}'`, function(err, result, fields) {
+      if(err) throw err;
+
+      console.log("Query successful");
+      req.session.uid = username;
+      res.redirect('/profile');
+    });
+    
 });
 
 app.get('/user', (req, res) => {
