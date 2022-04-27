29e3d0e91681a09af3638aafdb8f0debb49e9dac
Nathan Dodson <nathanrdodson@gmail.com>
2022-04-27 13:21:13 -0500

    change port for prod

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const bcrypt = require('bcrypt');
 var mysql = require('mysql');
 const { query } = require('express');
 
-const HTTP_PORT = 3000;
+const HTTP_PORT = 80;
 const SALT_ROUND = 12;
 
 var session;
